a505fff91cc2bc23a376ea256ac6ca74ccb02df9
jatin.batra <70570817+devops-4u@users.noreply.github.com>
2025-06-22 01:48:11 +0530

    Adding Task2 Files
    Adding Task2 Files

---
+++ eks-kubeconfig.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws eks update-kubeconfig --region ap-south-1 --name simpletimeservice-eks
---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.43.0, < 6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = ["${var.aws_region}a", "${var.aws_region}b"]
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  enable_nat_gateway   = true        
+  single_nat_gateway   = true  
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      min_capacity     = 1
+      max_capacity     = 3
+
+      instance_types = ["t3.small"]
+      subnet_ids     = module.vpc.private_subnets
+    }
+  }
+}
